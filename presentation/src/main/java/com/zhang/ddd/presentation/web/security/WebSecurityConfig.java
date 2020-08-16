5656ce7188766b6a78922e7220550a857d5b6f2a
zhang <zhang.xs.titech@gmail.com>
2020-08-16 22:33:18 +0900

    session timeout return 401

--- presentation/src/main/java/com/zhang/ddd/presentation/web/security/WebSecurityConfig.java
+++ presentation/src/main/java/com/zhang/ddd/presentation/web/security/WebSecurityConfig.java
@@ -5,10 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.web.cors.CorsConfiguration;
@@ -47,10 +49,12 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/api/user/me")
                 .permitAll()
                 .and()
+                //.anonymous().disable()
                 .csrf()
                 .disable()
-                //.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                ;
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+        //.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 
     @Bean
