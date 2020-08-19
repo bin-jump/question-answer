5eedede3a6b02c23533a925b352900da35d2821a
zhang <zhang.xs.titech@gmail.com>
2020-08-19 23:05:12 +0900

    config file adjust

--- infrastructure/src/main/java/com/zhang/ddd/infrastructure/search/config/Config.java
+++ infrastructure/src/main/java/com/zhang/ddd/infrastructure/search/config/Config.java
@@ -1,6 +1,8 @@
 package com.zhang.ddd.infrastructure.search.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -11,13 +13,23 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.zhang.ddd.infrastructure.search")
+@Slf4j
 public class Config {
+
+    @Value("${spring.data.elasticsearch.host}")
+    private String hostName;
+
+    @Value("${spring.data.elasticsearch.port}")
+    private int portNumber;
 
     @Bean
     public RestHighLevelClient client() {
+        String connectStr = hostName + ":" + portNumber;
+        log.info("elastic search: " + connectStr);
+
         ClientConfiguration clientConfiguration
                 = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo(connectStr)
                 .build();
 
         return RestClients.create(clientConfiguration).rest();
